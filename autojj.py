f373b60366b72829445aba34b85c20cf25c87607
maduma <maduma@pt.lu>
2020-03-21 18:03:23 +0100

    add logging

--- autojj.py
+++ autojj.py
@@ -9,19 +9,26 @@ UPDATE_JOB = 2
 CREATE_FOLDER = 3
 
 def next_api(job_exists, folder_exists, job_up_to_date=False):
+    api = {}
+
     if folder_exists:
         if job_exists:
             if job_up_to_date:
-                return {ACTION: NOP, GO_ON: False}
+                api = {ACTION: NOP, GO_ON: False}
             else:
-                return {ACTION: UPDATE_JOB, GO_ON: False}
+                api = {ACTION: UPDATE_JOB, GO_ON: False}
         else:
-            return {ACTION: CREATE_JOB, GO_ON: False}
+            api = {ACTION: CREATE_JOB, GO_ON: False}
     elif job_exists:
-        logging.error(f'Bad input: job_exists={job_exists} but not folder_exists={folder_exists}')
+        logging.error(f'Bad input: job_exists={job_exists} but not folder. folder_exists={folder_exists}')
+        api = {ACTION: NOP, GO_ON: False}
+    else:
+        api = {ACTION: CREATE_FOLDER, GO_ON: True}
+
+    if not api:
+        logging.error(f'Bad input: job_exists={job_exists} , folder_exists={folder_exists} , job_up_to_date={job_up_to_date}')
         return {ACTION: NOP, GO_ON: False}
     else:
-        return {ACTION: CREATE_FOLDER, GO_ON: True}
-
-    logging.error(f'Bad input: job_exists={job_exists} , folder_exists={folder_exists} , job_up_to_date={job_up_to_date}')
-    return {ACTION: NOP, GO_ON: False}
+        logging.debug(f'job_exists={job_exists} , folder_exists={folder_exists} , job_up_to_date={job_up_to_date}')
+        logging.info(f'ACTION: {api[ACTION]}, GO_ON: {api[GO_ON]}')
+        return api
