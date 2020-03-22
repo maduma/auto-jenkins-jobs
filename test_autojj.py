72233db034fc2169e9c6d8a43aadd1fea8fa45c5
Stéphane Nsakala <stephane.nsakala@luxairgroup.lu>
2020-03-22 15:03:54 +0100

    .

--- test_autojj.py
+++ test_autojj.py
@@ -5,9 +5,13 @@ import json
 def test_new_job_creation_folder():
     rest_api = next_action(job_exists=False, folder_exists=False)
     assert rest_api == { ACTION: CREATE_FOLDER, GO_ON: True }
+    rest_api = next_action(job_exists=False, folder_exists=False, job_up_to_date=True)
+    assert rest_api == { ACTION: CREATE_FOLDER, GO_ON: True }
 
 def test_new_job_creation_job():
     rest_api = next_action(job_exists=False, folder_exists=True)
+    assert rest_api == { ACTION: CREATE_JOB, GO_ON: False }
+    rest_api = next_action(job_exists=False, folder_exists=True, job_up_to_date=True)
     assert rest_api == { ACTION: CREATE_JOB, GO_ON: False }
 
 def test_update_job():
@@ -20,6 +24,8 @@ def test_job_fine():
 
 def test_bad_input1(): # job exists but not the folder !
     rest_api = next_action(job_exists=True, folder_exists=False)
+    assert rest_api == { ACTION: NOP, GO_ON: False }
+    rest_api = next_action(job_exists=True, folder_exists=False, job_up_to_date=True)
     assert rest_api == { ACTION: NOP, GO_ON: False }
 
 def test_get_job_repository_update_event():
