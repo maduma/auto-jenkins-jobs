382956a4b20931ab3cc2063cbdd624015c5aa3d8
maduma <maduma@pt.lu>
2020-04-20 17:14:50 +0200

    .

--- settings.py
+++ settings.py
@@ -2,12 +2,12 @@ import os
 
 AUTOJJ_VERSION = os.environ.get('AUTOJJ_VERSION', 'unknown')
 GITLAB_PRIVATE_TOKEN = os.environ.get('GITLAB_PRIVATE_TOKEN','unknown')
-JENKINS_URL = os.environ.get('JENKINS_URL', 'https://jenkins.maduma.org')
+JENKINS_URL = os.environ.get('JENKINS_URL', 'unknown')
 JENKINS_USERNAME = os.environ.get('JENKINS_USERNAME', 'unknown')
 JENKINS_PASSWORD = os.environ.get('JENKINS_PASSWORD', 'unknown')
 
 PROJECT_TYPES = os.environ.get('PROJECT_TYPES', 'mulePipeline').split(',')
 
-print(
-    f'''toto'''
-)
+print(JENKINS_URL)
+print(JENKINS_USERNAME)
+print(JENKINS_PASSWORD)
