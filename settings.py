02a0599d84bf4a12f4fa851e81b6b2abdbbf2b50
maduma <maduma@pt.lu>
2020-04-21 11:19:48 +0200

    .

--- settings.py
+++ settings.py
@@ -3,13 +3,16 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-AUTOJJ_VERSION = os.environ.get('AUTOJJ_VERSION', '__VERSION__')
+AUTOJJ_VERSION = '__VERSION__'
 GITLAB_PRIVATE_TOKEN = os.environ.get('GITLAB_PRIVATE_TOKEN','unknown')
 JENKINS_URL = os.environ.get('JENKINS_URL', 'unknown')
 JENKINS_USERNAME = os.environ.get('JENKINS_USERNAME', 'unknown')
 JENKINS_PASSWORD = os.environ.get('JENKINS_PASSWORD', 'unknown')
 PROJECT_TYPES = os.environ.get('PROJECT_TYPES', 'mulePipeline').split(',')
 
+logging.debug(AUTOJJ_VERSION)
+logging.debug(GITLAB_PRIVATE_TOKEN)
 logging.debug(JENKINS_URL)
 logging.debug(JENKINS_USERNAME)
 logging.debug(JENKINS_PASSWORD)
+logging.debug(PROJECT_TYPES)
